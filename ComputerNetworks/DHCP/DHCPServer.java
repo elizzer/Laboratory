363523dfea0b2f1e92a99e5ef2814f757898765d
elizzer <64605932+elizzer@users.noreply.github.com>
2022-11-28 02:39:13 +0530

    updated dhcp implementation

---
+++ ComputerNetworks/DHCP/DHCPServer.java
@@ -0,0 +1,63 @@
+package DHCP;
+import java.net.*;
+
+
+public class DHCPServer {
+    public static String MACTable[]= new String[100];
+    public static void main(String args[])throws Exception
+    {
+        String subNetIp="192.168.0.";
+        int nextIp=0;
+        InetAddress toIp=InetAddress.getByName("127.0.0.1");
+        String ComCodes[]={"111","110"};
+        DatagramSocket ds = new DatagramSocket(1234);
+        byte buf[]= new byte[1024];
+        DatagramPacket dpr = new DatagramPacket(buf, 1024);
+        while(true){
+
+            ds.receive(dpr);
+            int clientPort = dpr.getPort();
+            String str = new String(dpr.getData(),0,dpr.getLength());
+            if(str.equalsIgnoreCase(ComCodes[0])){
+                System.out.println("[+]New client asking for ip...");
+                DatagramPacket dps = new DatagramPacket(ComCodes[1].getBytes(),ComCodes[1].length(),toIp,clientPort);
+                ds.send(dps);
+                ds.receive(dpr);
+                str= new String(dpr.getData(),0,dpr.getLength());
+                System.out.println("[+]Mac of the client is "+str);
+                int ip= findIp(str);
+                setIp(ip, str);
+                String finalIp=subNetIp+ip;
+                dps = new DatagramPacket(finalIp.getBytes(),finalIp.length(),toIp,clientPort);
+                ds.send(dps);
+            }
+        }
+            // System.out.println(str);
+        // ds.close();
+    }    
+
+    public static int findIp(String mac){
+        int ip=-1;
+        for(int i=0;i<100;i++){
+            if(MACTable[i]==null){
+                // System.out.println("[+]"+i);
+                if(ip==-1){
+                    ip=i;
+                }
+            }
+            else{
+                // System.out.println("[+]TT"+MACTable[i]);
+                if(MACTable[i].equals(mac)){
+                    return i;
+                }
+            }
+        }
+
+        return ip;
+        
+    }
+    public static void setIp(int ip,String mac){
+        MACTable[ip]=mac;
+        
+    }
+}
---
+++ ComputerNetworks/DHCP/WifiClient.java
@@ -0,0 +1,39 @@
+package DHCP;
+import java.io.InputStream;
+import java.net.*;
+import java.util.Scanner;
+
+public class WifiClient {
+    public static void main(String args[])throws Exception
+    {
+        String subNetIp="192.168.0.";
+        int nextIp=0;
+        int clientPort=1235,dhcpPort=1234;
+        DatagramSocket ds = new DatagramSocket(clientPort);
+        String ComCodes[]={"111","110"};
+
+        String mac="aa:aa:aa:aa:aa:aa:aa:aa";
+        System.out.println("[+]Enter the mac of the device:");
+        Scanner sin = new Scanner(System.in);
+        mac = sin.next();
+        InetAddress toIp= InetAddress.getByName("127.0.0.1");
+        DatagramPacket dp = new DatagramPacket(ComCodes[0].getBytes(),ComCodes[0].length(),toIp,dhcpPort);
+        ds.send(dp);
+        byte buf[]= new byte[1024];
+        DatagramPacket dpr = new DatagramPacket(buf, 1024);
+        ds.receive(dpr);
+        String str = new String(dpr.getData(),0,dpr.getLength());
+        if(str.equals(ComCodes[1])){
+            System.out.println("[+]Ip config req accepted...");
+            System.out.println("[+]DHCP is asking for the MAC");
+            dp=new DatagramPacket(mac.getBytes(),mac.length(),toIp,dhcpPort);
+            ds.send(dp);
+            ds.receive(dpr);
+            str=new String(dpr.getData(),0,dpr.getLength());
+            System.out.println("[+]Assigned Ip "+str);
+        }
+
+
+        ds.close();
+    }    
+}
